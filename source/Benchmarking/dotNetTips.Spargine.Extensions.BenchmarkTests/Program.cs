fda1a893c91f05556ba1f0be414e281de6607d60
David McCarter <davidmccarter@live.com>
2024-07-28 09:14:12 -0700

    Update versions and release notes for August 2024. Final changes for AUG 2024 release.
    Updated version numbers to 2024.8.8.1 across multiple project files, including `README.md`, `.csproj` files, and `Program.cs`. Updated `PackageReleaseNotes` to reflect major performance and documentation changes in August 2024. Modified `Program.cs` to run all benchmarks using `BenchmarkSwitcher`.

--- source/Benchmarking/dotNetTips.Spargine.Extensions.BenchmarkTests/Program.cs
+++ source/Benchmarking/dotNetTips.Spargine.Extensions.BenchmarkTests/Program.cs
@@ -46,7 +46,7 @@ public static class Program
 
 			config = config.WithOption(ConfigOptions.DisableOptimizationsValidator, true);
 
-			//_ = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).RunAll(config);
+			_ = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).RunAll(config);
 
 			//_ = BenchmarkRunner.Run<ArrayExtensionsCollectionBenchmark>(config);
 			//_ = BenchmarkRunner.Run<AssemblyExtensionsBenchmark>(config);
@@ -63,10 +63,10 @@ public static class Program
 			//_ = BenchmarkRunner.Run<SortedDictionaryExtensionsBenchmark>(config);
 			//_ = BenchmarkRunner.Run<SortedSetCollectionBenchmark>(config);
 			//_ = BenchmarkRunner.Run<StringBuilderExtensionsCounterBenchmark>(config);
-			_ = BenchmarkRunner.Run<TypeExtensionsBenchmark>(config);
+			//_ = BenchmarkRunner.Run<TypeExtensionsBenchmark>(config);
 			//_ = BenchmarkRunner.Run<ReadOnlyCollectionBenchmark>(config);
 			//_ = BenchmarkRunner.Run<StringExtensionsBenchmark>(config);
-			_ = BenchmarkRunner.Run<StringExtensionsCounterBenchmark>(config);
+			//_ = BenchmarkRunner.Run<StringExtensionsCounterBenchmark>(config);
 
 			ConsoleLogger.Default.WriteLine(CompleteMessage);
 			Console.Beep();
