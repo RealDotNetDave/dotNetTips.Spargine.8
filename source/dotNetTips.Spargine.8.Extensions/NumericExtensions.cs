2494c0d9a4e4f1b766ba3631efa0898d3fb43072
David McCarter <davidmccarter@live.com>
2024-09-24 13:38:53 -0700

    Update modification date in NumericExtensions.cs header
    Updated the modification date in the file header comment of
    NumericExtensions.cs from September 21, 2024, to September 24, 2024.

--- source/dotNetTips.Spargine.8.Extensions/NumericExtensions.cs
+++ source/dotNetTips.Spargine.8.Extensions/NumericExtensions.cs
@@ -4,7 +4,7 @@
 // Created          : 12-17-2020
 //
 // Last Modified By : David McCarter
-// Last Modified On : 09-21-2024
+// Last Modified On : 09-24-2024
 // ***********************************************************************
 // <copyright file="NumericExtensions.cs" company="McCarter Consulting">
 //     Copyright (c) David McCarter - dotNetTips.com. All rights reserved.
