37b0c0084a169153376405031d58fc38e8775b95
David McCarter <dotnetdave@live.com>
2025-01-02 13:59:45 -0600

    Remove StreamExtensions async methods and related tests
    Updated file header date in StreamExtensionsTests.cs. Removed helper and test methods related to stream operations. Removed using directives and async ReadAsync and WriteAsync methods from StreamExtensions.cs.

--- source/Unit Tests/dotNetTips.Spargine.Extensions.Tests/StreamExtensionsTests.cs
+++ source/Unit Tests/dotNetTips.Spargine.Extensions.Tests/StreamExtensionsTests.cs
@@ -4,7 +4,7 @@
 // Created          : 10-09-2024
 //
 // Last Modified By : david
-// Last Modified On : 10-28-2024
+// Last Modified On : 01-02-2025
 // ***********************************************************************
 // <copyright file="StreamExtensionsTests.cs" company="DotNetTips.Spargine.Extensions.Tests">
 //     Copyright (c) . All rights reserved.
@@ -26,19 +26,6 @@ namespace DotNetTips.Spargine.Extensions.Tests;
 [TestClass]
 public class StreamExtensionsTests
 {
-
-	// Helper method to create a buffer of specified length
-	private byte[] CreateBuffer(int length)
-	{
-		return new byte[length];
-	}
-
-	// Helper method to create a MemoryStream with specified data
-	private MemoryStream CreateStream(byte[] data)
-	{
-		return new MemoryStream(data);
-	}
-
 	[TestMethod]
 	public void FlushClose_StreamIsNotNull_FlushesAndClosesStream()
 	{
@@ -60,105 +47,5 @@ public class StreamExtensionsTests
 
 		// Act & Assert
 		stream.FlushClose(); // Should not throw any exception
-	}
-
-	[TestMethod]
-	public async Task ReadAsync_CancellationRequested_ThrowsTaskCanceledException()
-	{
-		// Arrange
-		var data = new byte[] { 1, 2, 3, 4, 5 };
-		using var stream = new MemoryStream(data);
-		var buffer = new byte[data.Length];
-		var cts = new CancellationTokenSource();
-		cts.Cancel();
-
-		// Act & Assert
-		await Assert.ThrowsExceptionAsync<TaskCanceledException>(async () =>
-		{
-			await stream.ReadAsync(buffer, cts.Token);
-		});
-	}
-
-	[TestMethod]
-	public async Task ReadAsync_EmptyStream_ReturnsZero()
-	{
-		// Arrange
-		using var stream = new MemoryStream();
-		var buffer = new byte[10];
-
-		// Act
-		var bytesRead = await stream.ReadAsync(buffer, CancellationToken.None);
-
-		// Assert
-		Assert.AreEqual(0, bytesRead);
-	}
-	[TestMethod]
-	public async Task ReadAsync_ValidData_ReadsCorrectly()
-	{
-		// Arrange
-		var data = new byte[] { 1, 2, 3, 4, 5 };
-		using var stream = new MemoryStream(data);
-		var buffer = new byte[data.Length];
-
-		// Act
-		var bytesRead = await stream.ReadAsync(buffer, CancellationToken.None);
-
-		// Assert
-		Assert.AreEqual(data.Length, bytesRead);
-		CollectionAssert.AreEqual(data, buffer);
-	}
-
-	[TestMethod]
-	public async Task WriteAsync_CancellationRequested_ThrowsTaskCanceledException()
-	{
-		// Arrange
-		var data = new byte[] { 1, 2, 3, 4, 5 };
-		using var stream = new MemoryStream();
-		var source = new ReadOnlyMemory<byte>(data);
-		var cts = new CancellationTokenSource();
-		cts.Cancel();
-
-		// Act & Assert
-		await Assert.ThrowsExceptionAsync<TaskCanceledException>(async () =>
-		{
-			await stream.WriteAsync(source, cts.Token);
-		});
-	}
-
-	[TestMethod]
-	public async Task WriteAsync_EmptyData_WritesNothing()
-	{
-		// Arrange
-		var data = Array.Empty<byte>();
-		using var stream = new MemoryStream();
-		var source = new ReadOnlyMemory<byte>(data);
-
-		// Act
-		await stream.WriteAsync(source, CancellationToken.None);
-		stream.Position = 0;
-		var buffer = new byte[10];
-		var bytesRead = await stream.ReadAsync(buffer, CancellationToken.None);
-
-		// Assert
-		Assert.AreEqual(0, bytesRead);
-	}
-
-
-	[TestMethod]
-	public async Task WriteAsync_ValidData_WritesCorrectly()
-	{
-		// Arrange
-		var data = new byte[] { 1, 2, 3, 4, 5 };
-		using var stream = new MemoryStream();
-		var source = new ReadOnlyMemory<byte>(data);
-
-		// Act
-		await stream.WriteAsync(source, CancellationToken.None);
-		stream.Position = 0;
-		var buffer = new byte[data.Length];
-		await stream.ReadAsync(buffer, CancellationToken.None);
-
-		// Assert
-		CollectionAssert.AreEqual(data, buffer);
 	}
 }
--- source/dotNetTips.Spargine.8.Extensions/StreamExtensions.cs
+++ source/dotNetTips.Spargine.8.Extensions/StreamExtensions.cs
@@ -11,9 +11,6 @@
 // </copyright>
 // <summary>Extension methods designed for Stream.</summary>
 // ***********************************************************************
-using System.Buffers;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using DotNetTips.Spargine.Core;
 
 //`![Spargine 8 -  #RockYourCode](6219C891F6330C65927FA249E739AC1F.png;https://bit.ly/Spargine )
@@ -41,85 +38,4 @@ public static class StreamExtensions
 			stream.Close();
 		}
 	}
-
-	/// <summary>
-	/// Reads from the <see cref="Stream"/> asynchronously.
-	/// Validates that <paramref name="stream"/> is not null.
-	/// </summary>
-	/// <param name="stream">The stream to read from.</param>
-	/// <param name="destination">The memory region to fill with the data read from the stream.</param>
-	/// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is <see cref="CancellationToken.None"/>.</param>
-	/// <returns>A task that represents the asynchronous read operation. The value of the TResult parameter contains the total number of bytes read into the buffer.</returns>
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	[Information("From .NET Core source.", author: "David McCarter", createdOn: "7/15/2020", UnitTestStatus = UnitTestStatus.None, OptimizationStatus = OptimizationStatus.None, BenchmarkStatus = BenchmarkStatus.None, Status = Status.Available)]
-	public static ValueTask<int> ReadAsync(this Stream stream, Memory<byte> destination, CancellationToken cancellationToken = default)
-	{
-		stream = stream.ArgumentNotNull();
-		destination = destination.ArgumentNotNull();
-
-		if (MemoryMarshal.TryGetArray(destination, out ArraySegment<byte> array))
-		{
-			return new ValueTask<int>(stream.ReadAsync(array.Array, array.Offset, array.Count, cancellationToken));
-		}
-		else
-		{
-			var buffer = ArrayPool<byte>.Shared.Rent(destination.Length);
-
-			return new ValueTask<int>(FinishReadAsync(stream.ReadAsync(buffer, 0, destination.Length, cancellationToken), buffer, destination));
-
-			static async Task<int> FinishReadAsync(Task<int> readTask, byte[] localBuffer, Memory<byte> localDestination)
-			{
-				try
-				{
-					var result = await readTask.ConfigureAwait(false);
-					new Span<byte>(localBuffer, 0, result).CopyTo(localDestination.Span);
-					return result;
-				}
-				finally
-				{
-					ArrayPool<byte>.Shared.Return(localBuffer);
-				}
-			}
-		}
-	}
-
-	/// <summary>
-	/// Writes to the <see cref="Stream"/> asynchronously.
-	/// Validates that <paramref name="stream"/> is not null.
-	/// </summary>
-	/// <param name="stream">The <see cref="Stream"/> to write to.</param>
-	/// <param name="source">The data to write to the stream.</param>
-	/// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is <see cref="CancellationToken.None"/>.</param>
-	/// <returns>A <see cref="ValueTask"/> that represents the asynchronous write operation.</returns>
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	[Information("From .NET Core source.", author: "David McCarter", createdOn: "7/15/2020", UnitTestStatus = UnitTestStatus.Completed, OptimizationStatus = OptimizationStatus.None, BenchmarkStatus = BenchmarkStatus.None, Status = Status.Available)]
-	public static ValueTask WriteAsync(this Stream stream, ReadOnlyMemory<byte> source, CancellationToken cancellationToken = default)
-	{
-		stream = stream.ArgumentNotNull();
-
-		if (MemoryMarshal.TryGetArray(source.ArgumentNotNull(), out var array))
-		{
-			return new ValueTask(stream.WriteAsync(array.Array, array.Offset, array.Count, cancellationToken));
-		}
-		else
-		{
-			var buffer = ArrayPool<byte>.Shared.Rent(source.Length);
-			source.Span.CopyTo(buffer);
-
-			return new ValueTask(FinishWriteAsync(stream.WriteAsync(buffer, 0, source.Length, cancellationToken), buffer));
-
-			static async Task FinishWriteAsync(Task writeTask, byte[] localBuffer)
-			{
-				try
-				{
-					await writeTask.ConfigureAwait(false);
-				}
-				finally
-				{
-					ArrayPool<byte>.Shared.Return(localBuffer);
-				}
-			}
-		}
-	}
-
 }
