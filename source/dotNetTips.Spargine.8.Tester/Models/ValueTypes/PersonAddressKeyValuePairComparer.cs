fdf6c98064b421af83dc94e9bde521b2f2b6eb0a
David McCarter <dotnetdave@live.com>
2025-01-14 13:40:17 -0600

    Add comparers for Person<Address> and KeyValuePair types
    Modified binary file `Spargine Types.xlsx` in `Spargine-Charts`.
    
    Added `PersonAddressComparer.cs` and `PersonAddressKeyValuePairComparer.cs` to `DotNetTips.Spargine.Tester.Models.RefTypes` and `DotNetTips.Spargine.Tester.Models.ValueTypes` namespaces.
    - Included metadata headers, copyright info, and summaries.
    - Referenced `DotNetTips.Spargine.Core` namespace.
    - Added `[Information(Status = Status.New)]` attribute.
    - Implemented `IComparer<T>` interface.
    - `PersonAddressComparer` compares `Person<Address>` objects by `FirstName`.
    - `PersonAddressKeyValuePairComparer` compares `KeyValuePair<string, Person<Address>>` by `FirstName` of `Person<Address>`.

---
+++ source/dotNetTips.Spargine.8.Tester/Models/ValueTypes/PersonAddressKeyValuePairComparer.cs
@@ -0,0 +1,50 @@
+// ***********************************************************************
+// Assembly         : DotNetTips.Spargine.8.Tester
+// Author           : David McCarter
+// Created          : 01-14-2025
+//
+// Last Modified By : David McCarter
+// Last Modified On : 01-14-2025
+// ***********************************************************************
+// <copyright file="PersonAddressKeyValuePairComparer.cs" company="David McCarter - dotNetTips.com">
+//     McCarter Consulting (David McCarter)
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+using DotNetTips.Spargine.Core;
+
+//`![Spargine 8 -  #RockYourCode](6219C891F6330C65927FA249E739AC1F.png;https://bit.ly/Spargine )
+
+namespace DotNetTips.Spargine.Tester.Models.ValueTypes;
+
+/// <summary>
+/// Provides a comparison for <see cref="KeyValuePair{TKey, TValue}"/> objects where the value is a <see cref="Person{Address}"/>.
+/// </summary>
+[Information(Status = Status.New)]
+public class PersonAddressKeyValuePairComparer : IComparer<KeyValuePair<string, Person<Address>>>
+{
+	/// <summary>
+	/// Compares two <see cref="KeyValuePair{TKey, TValue}"/> objects and returns a value indicating whether one is less than, equal to, or greater than the other.
+	/// </summary>
+	/// <param name="x">The first <see cref="KeyValuePair{TKey, TValue}"/> to compare.</param>
+	/// <param name="y">The second <see cref="KeyValuePair{TKey, TValue}"/> to compare.</param>
+	/// <returns>
+	/// A signed integer that indicates the relative values of <paramref name="x"/> and <paramref name="y"/>:
+	/// <list type="bullet">
+	/// <item>
+	/// <description>Less than zero: <paramref name="x"/> is less than <paramref name="y"/>.</description>
+	/// </item>
+	/// <item>
+	/// <description>Zero: <paramref name="x"/> equals <paramref name="y"/>.</description>
+	/// </item>
+	/// <item>
+	/// <description>Greater than zero: <paramref name="x"/> is greater than <paramref name="y"/>.</description>
+	/// </item>
+	/// </list>
+	/// </returns>
+	public int Compare(KeyValuePair<string, Person<Address>> x, KeyValuePair<string, Person<Address>> y)
+	{
+		return string.Compare(x.Value.FirstName, y.Value.FirstName, StringComparison.Ordinal);
+	}
+}
