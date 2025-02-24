1c39c730a0ea84413e03539d8c315d61908f3387
David McCarter <dotnetdave@live.com>
2025-02-24 10:39:20 -0600

    Add ConcurrentBagExtensions with new methods
    Updated GlobalSuppressions.cs to suppress warnings for
    the ToList method in ConcurrentBagExtensions. Created
    ConcurrentBagExtensions.cs with extension methods for
    ConcurrentBag<T>, including AddItems, RemoveItems,
    and ToList. Each method is documented with XML comments
    and includes metadata attributes.

---
+++ source/dotNetTips.Spargine.8.Extensions/ConcurrentBagExtensions.cs
@@ -0,0 +1,90 @@
+// ***********************************************************************
+// Assembly         : DotNetTips.Spargine.8.Extensions
+// Author           : David McCarter
+// Created          : 02-24-2025
+//
+// Last Modified By : David McCarter
+// Last Modified On : 02-24-2025
+// ***********************************************************************
+// <copyright file="ConcurrentBagExtensions.cs" company="David McCarter - dotNetTips.com">
+//     McCarter Consulting (David McCarter)
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+using System.Collections.Concurrent;
+using DotNetTips.Spargine.Core;
+using Microsoft.VisualBasic;
+
+//`![Spargine 8 -  #RockYourCode](6219C891F6330C65927FA249E739AC1F.png;https://bit.ly/Spargine )
+
+namespace DotNetTips.Spargine.Extensions;
+
+/// <summary>
+/// Provides extension methods for <see cref="ConcurrentBag{T}"/>.
+/// </summary>
+[Information(Status = Status.New)]
+public static class ConcurrentBagExtensions
+{
+
+	/// <summary>
+	/// Adds a range of items to the <see cref="ConcurrentBag{T}"/>.
+	/// </summary>
+	/// <typeparam name="T">The type of elements in the bag.</typeparam>
+	/// <param name="bag">The <see cref="ConcurrentBag{T}"/> to which to add items. Must not be null.</param>
+	/// <param name="items">The items to add to the bag. Must not be null.</param>
+	/// <exception cref="ArgumentNullException">Thrown if <paramref name="bag"/> or <paramref name="items"/> is null.</exception>
+	[Information(nameof(AddItems), "David McCarter", "2/24/2025", OptimizationStatus = OptimizationStatus.None, BenchmarkStatus = BenchmarkStatus.Benchmark, UnitTestStatus = UnitTestStatus.None, Status = Status.New)]
+	public static void AddItems<T>(this ConcurrentBag<T> bag, IEnumerable<T> items)
+	{
+		bag = bag.ArgumentNotNull();
+		items = items.ArgumentNotNull();
+
+		foreach (var item in items)
+		{
+			bag.Add(item);
+		}
+	}
+
+	/// <summary>
+	/// Removes the specified items from the <see cref="ConcurrentBag{T}"/>.
+	/// </summary>
+	/// <typeparam name="T">The type of elements in the bag.</typeparam>
+	/// <param name="bag">The <see cref="ConcurrentBag{T}"/> from which to remove items. Must not be null.</param>
+	/// <param name="items">The items to remove from the bag. Must not be null.</param>
+	/// <returns>A new <see cref="ConcurrentBag{T}"/> with the specified items removed.</returns>
+	/// <exception cref="ArgumentNullException">Thrown if <paramref name="bag"/> or <paramref name="items"/> is null.</exception>
+	[Information(nameof(RemoveItems), "David McCarter", "2/24/2025", OptimizationStatus = OptimizationStatus.None, BenchmarkStatus = BenchmarkStatus.Benchmark, UnitTestStatus = UnitTestStatus.None, Status = Status.New)]
+	public static ConcurrentBag<T> RemoveItems<T>(this ConcurrentBag<T> bag, IEnumerable<T> items)
+	{
+		bag = bag.ArgumentNotNull();
+		items = items.ArgumentNotNull();
+
+		var tempBag = new ConcurrentBag<T>();
+
+		_ = Parallel.ForEach(bag, item =>
+		{
+			if (!items.Contains(item))
+			{
+				tempBag.Add(item);
+			}
+		});
+
+		return tempBag;
+	}
+
+	/// <summary>
+	/// Converts the <see cref="ConcurrentBag{T}"/> to a <see cref="List{T}"/>.
+	/// </summary>
+	/// <typeparam name="T">The type of elements in the bag.</typeparam>
+	/// <param name="bag">The <see cref="ConcurrentBag{T}"/> to convert. Must not be null.</param>
+	/// <returns>A <see cref="List{T}"/> containing the elements of the bag.</returns>
+	/// <exception cref="ArgumentNullException">Thrown if <paramref name="bag"/> is null.</exception>
+	[Information(nameof(ToList), "David McCarter", "2/24/2025", OptimizationStatus = OptimizationStatus.None, BenchmarkStatus = BenchmarkStatus.Benchmark, UnitTestStatus = UnitTestStatus.None, Status = Status.New)]
+	public static List<T> ToList<T>(this ConcurrentBag<T> bag)
+	{
+		bag = bag.ArgumentNotNull();
+
+		return [.. bag];
+	}
+}
