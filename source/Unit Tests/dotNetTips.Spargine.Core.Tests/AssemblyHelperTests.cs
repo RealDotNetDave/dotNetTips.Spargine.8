16cbe36ce03c2941ccd260d2ffd7697b1f16b163
David McCarter <dotnetdave@live.com>
2025-04-09 12:33:16 -0500

    Rename FindNetSDKFiles to GetNetSdkDllFiles
    Updated AssemblyHelperTests to use GetNetSdkDllFiles and added new tests for FindTypesImplementing and LoadAssemblyTypes.
    Renamed FindNetSDKFiles in AssemblyHelper.cs and modified its implementation to search DLLs in all directories.
    Updated GlobalSuppressions.cs to reflect method name changes.

--- source/Unit Tests/dotNetTips.Spargine.Core.Tests/AssemblyHelperTests.cs
+++ source/Unit Tests/dotNetTips.Spargine.Core.Tests/AssemblyHelperTests.cs
@@ -13,6 +13,7 @@
 // ***********************************************************************
 
 using System;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 //`![Spargine 8 -  #RockYourCode](6219C891F6330C65927FA249E739AC1F.png;https://bit.ly/Spargine )
@@ -26,7 +27,7 @@ public class AssemblyHelperTests
 	public void FindNetSDKFiles_DotnetRootSet_ReturnsFiles()
 	{
 		// Act
-		var result = AssemblyHelper.FindNetSDKFiles(null);
+		var result = AssemblyHelper.GetNetSdkDllFiles(null);
 
 		// Assert
 		Assert.IsNotNull(result);
@@ -37,7 +38,7 @@ public class AssemblyHelperTests
 	public void FindNetSDKFiles_NoMatchingVersion_ReturnsEmpty()
 	{
 		// Act
-		var result = AssemblyHelper.FindNetSDKFiles("99.99.99");
+		var result = AssemblyHelper.GetNetSdkDllFiles("99.99.99");
 
 		// Assert
 		Assert.IsNotNull(result);
@@ -48,11 +49,112 @@ public class AssemblyHelperTests
 	public void FindNetSDKFiles_SpecificVersion_ReturnsFiles()
 	{
 		// Act
-		var result = AssemblyHelper.FindNetSDKFiles("8.0.15");
+		var result = AssemblyHelper.GetNetSdkDllFiles("8.0.15");
 
 		// Assert
 		Assert.IsNotNull(result);
 		Assert.IsTrue(result.Count > 0, "Expected to find .NET SDK files for version 8.0.15, but none were found.");
 	}
+
+	[TestMethod]
+	public void FindTypesImplementing_NoMatchingTypes_ReturnsEmpty()
+	{
+		// Arrange
+		var assemblyFile = new FileInfo(typeof(AssemblyHelper).Assembly.Location);
+
+		// Act
+		var result = AssemblyHelper.FindTypesImplementing(assemblyFile, typeof(ICloneable));
+
+		// Assert
+		Assert.IsNotNull(result);
+		Assert.AreEqual(0, result.Count, "Expected no types implementing ICloneable, but some were found.");
+	}
+	[TestMethod]
+	[ExpectedException(typeof(ArgumentNullException))]
+	public void FindTypesImplementing_NullFile_ThrowsArgumentNullException()
+	{
+		// Act
+		_ = AssemblyHelper.FindTypesImplementing(null!, typeof(IDisposable));
+	}
+
+	[TestMethod]
+	[ExpectedException(typeof(ArgumentNullException))]
+	public void FindTypesImplementing_NullTypeNames_ThrowsArgumentNullException()
+	{
+		// Arrange
+		var assemblyFile = new FileInfo(typeof(AssemblyHelper).Assembly.Location);
+
+		// Act
+		_ = AssemblyHelper.FindTypesImplementing(assemblyFile, null!);
+	}
+
+	[TestMethod]
+	public void FindTypesImplementing_ValidAssembly_ReturnsMatchingTypes()
+	{
+		// Arrange
+		var assemblyFile = new FileInfo(typeof(AssemblyHelper).Assembly.Location);
+
+		// Act
+		var result = AssemblyHelper.FindTypesImplementing(assemblyFile, typeof(Attribute));
+
+		// Assert
+		Assert.IsNotNull(result);
+		Assert.IsTrue(result.Count > 0, "Expected to find types implementing Attribute, but none were found.");
+	}
+
+	[TestMethod]
+	[ExpectedException(typeof(BadImageFormatException))]
+	public void LoadAssemblyTypes_InvalidAssembly_ThrowsBadImageFormatException()
+	{
+		// Arrange
+		var tempFile = Path.GetTempFileName();
+
+		try
+		{
+			var invalidAssemblyFile = new FileInfo(tempFile);
+
+			// Act
+			_ = AssemblyHelper.LoadAssemblyTypes(invalidAssemblyFile);
+		}
+		finally
+		{
+			// Cleanup
+			File.Delete(tempFile);
+		}
+	}
+
+	[TestMethod]
+	[ExpectedException(typeof(FileNotFoundException))]
+	public void LoadAssemblyTypes_NonExistentFile_ThrowsFileNotFoundException()
+	{
+		// Arrange
+		var nonExistentFile = new FileInfo("NonExistentAssembly.dll");
+
+		// Act
+		_ = AssemblyHelper.LoadAssemblyTypes(nonExistentFile);
+	}
+
+	[TestMethod]
+	[ExpectedException(typeof(ArgumentNullException))]
+	public void LoadAssemblyTypes_NullFile_ThrowsArgumentNullException()
+	{
+		// Act
+		_ = AssemblyHelper.LoadAssemblyTypes(null!);
+	}
+
+	[TestMethod]
+	public void LoadAssemblyTypes_ValidAssembly_ReturnsTypes()
+	{
+		// Arrange
+		var assemblyFile = new FileInfo(typeof(AssemblyHelper).Assembly.Location);
+
+		// Act
+		var result = AssemblyHelper.LoadAssemblyTypes(assemblyFile);
+
+		// Assert
+		Assert.IsNotNull(result);
+		Assert.IsTrue(result.Count > 0, "Expected to find types in the assembly, but none were found.");
+	}
+
 }
 
--- source/dotNetTips.Spargine.8.Core/AssemblyHelper.cs
+++ source/dotNetTips.Spargine.8.Core/AssemblyHelper.cs
@@ -15,6 +15,8 @@
 
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
 using DotNetTips.Spargine.Core.RegularExpressions;
 
 namespace DotNetTips.Spargine.Core;
@@ -29,7 +31,7 @@ public static class AssemblyHelper
 	/// <param name="input">The version string to parse.</param>
 	/// <returns>A <see cref="Version"/> object representing the parsed version, or <c>Version(0,0)</c> if parsing fails.</returns>
 	/// <remarks>
-	/// This method attempts to parse the input string as a version. If the parsing fails, it tries to extract a version-like pattern
+	/// This method attempts to parse the file string as a version. If the parsing fails, it tries to extract a version-like pattern
 	/// using a regular expression and parses that. If all attempts fail, it returns <c>Version(0,0)</c>.
 	/// </remarks>
 	/// <exception cref="ArgumentNullException">Thrown if <paramref name="input"/> is null.</exception>
@@ -52,6 +54,40 @@ public static class AssemblyHelper
 	}
 
 	/// <summary>
+	/// Finds all types in the specified assembly file that implement or inherit from any of the specified types.
+	/// </summary>
+	/// <param name="file">The <see cref="FileInfo"/> representing the assembly file to search.</param>
+	/// <param name="typeNames">An array of <see cref="Type"/> objects to check for implementation or inheritance.</param>
+	/// <returns>A read-only collection of <see cref="Type"/> objects that implement or inherit from the specified types.</returns>
+	/// <exception cref="ArgumentNullException">Thrown if <paramref name="file"/> or <paramref name="typeNames"/> is null.</exception>
+	/// <exception cref="FileNotFoundException">Thrown if the specified assembly file does not exist.</exception>
+	[Information(nameof(FindTypesImplementing), UnitTestStatus = UnitTestStatus.None, Status = Status.New)]
+	public static ReadOnlyCollection<Type> FindTypesImplementing([NotNull] this FileInfo file, params Type[] typeNames)
+	{
+		file = file.ArgumentNotNull();
+		typeNames = typeNames.ArgumentNotNull();
+		var matchingTypes = new List<Type>();
+
+		//Load all types
+		var types = LoadAssemblyTypes(file);
+
+		foreach (var type in types)
+		{
+			foreach (var targetType in typeNames)
+			{
+				// Check if the type implements or inherits from the target type
+				if (targetType.IsAssignableFrom(type) && type != targetType)
+				{
+					matchingTypes.Add(type);
+					break; // No need to check further once a match is found
+				}
+			}
+		}
+
+		return new ReadOnlyCollection<Type>(matchingTypes);
+	}
+
+	/// <summary>
 	/// Finds .NET SDK files in the specified version or the highest available version.
 	/// </summary>
 	/// <param name="version">The specific version to search for. If null, the highest version is used.</param>
@@ -61,8 +97,8 @@ public static class AssemblyHelper
 	/// It retrieves DLL files from the "ref" folder of the selected version and target framework.
 	/// </remarks>
 	/// <exception cref="DirectoryNotFoundException">Thrown if the .NET packs directory does not exist.</exception>
-	[Information(nameof(FindNetSDKFiles), "David McCarter", "4/9/2025", UnitTestStatus = UnitTestStatus.None, BenchmarkStatus = BenchmarkStatus.NotRequired, Status = Status.New)]
-	public static ReadOnlyCollection<FileInfo> FindNetSDKFiles(string? version)
+	[Information(nameof(GetNetSdkDllFiles), "David McCarter", "4/9/2025", UnitTestStatus = UnitTestStatus.Completed, BenchmarkStatus = BenchmarkStatus.NotRequired, Status = Status.New)]
+	public static ReadOnlyCollection<FileInfo> GetNetSdkDllFiles(string? version)
 	{
 		var root = Environment.GetEnvironmentVariable("DOTNET_ROOT");
 
@@ -113,7 +149,9 @@ public static class AssemblyHelper
 
 					if (selectedTf != null)
 					{
-						var dllPaths = selectedTf.GetFiles("*.dll");
+						// Get all DLL files in the "ref" folder of the selected version and target framework
+						var dllPaths = selectedTf.GetFiles("*.dll", SearchOption.AllDirectories);
+
 						dllFiles.AddRange(dllPaths);
 					}
 				}
@@ -122,4 +160,37 @@ public static class AssemblyHelper
 
 		return new ReadOnlyCollection<FileInfo>(dllFiles);
 	}
+
+	/// <summary>
+	/// Loads the assembly from the specified file and extracts all types within it.
+	/// </summary>
+	/// <param name="assemblyFile">The <see cref="FileInfo"/> representing the assembly file to load.</param>
+	/// <returns>A collection of <see cref="Type"/> objects representing all types in the assembly.</returns>
+	/// <exception cref="ArgumentNullException">Thrown if <paramref name="assemblyFile"/> is null.</exception>
+	/// <exception cref="FileNotFoundException">Thrown if the specified assembly file does not exist.</exception>
+	/// <exception cref="BadImageFormatException">Thrown if the file is not a valid .NET assembly.</exception>
+	/// <exception cref="ReflectionTypeLoadException">Thrown if some types in the assembly cannot be loaded.</exception>
+	[Information(nameof(LoadAssemblyTypes), "David McCarter", "4/9/2025", UnitTestStatus = UnitTestStatus.Completed, BenchmarkStatus = BenchmarkStatus.NotRequired, Status = Status.New)]
+	public static ReadOnlyCollection<Type> LoadAssemblyTypes(FileInfo assemblyFile)
+	{
+		assemblyFile = assemblyFile.ArgumentExists(assemblyFile);
+
+		try
+		{
+			var assembly = Assembly.LoadFrom(assemblyFile.FullName);
+			return assembly.GetTypes().AsReadOnly();
+		}
+		catch (ReflectionTypeLoadException ex)
+		{
+			// Return the types that were successfully loaded and log the loader exceptions if needed.
+			Trace.WriteLine($"Failed to load some types from assembly: {assemblyFile.FullName}");
+			foreach (var loaderException in ex.LoaderExceptions)
+			{
+				Trace.WriteLine(loaderException?.Message);
+			}
+
+			return ex.Types.Where(type => type != null).Select(type => type!).ToList().AsReadOnly();
+		}
+	}
+
 }
